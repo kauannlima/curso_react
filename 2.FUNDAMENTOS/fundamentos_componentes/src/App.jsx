dc3ed0cf504ad08cbc151865fb3310bfb7e811c0
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-04-15 22:46:32 -0300

    Entendendo Template Expressions

--- 2.FUNDAMENTOS/fundamentos_componentes/src/App.jsx
+++ 2.FUNDAMENTOS/fundamentos_componentes/src/App.jsx
@@ -3,12 +3,14 @@ import FirstComponent from './components/FirstComponent'
 
 //styles / css
 import './App.css'
+import TemplateExpressions from './components/TemplateExpressions.jsx'
 
 function App() {
   return (
     <>
     <h1>Fundamentos React</h1>
     <FirstComponent/>
+    <TemplateExpressions/>
     </>
   )
 }
