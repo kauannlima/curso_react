6859b4660b25a7356543b515c9c2213cdcd9ba0a
kauannlima <124539523+kauannlima@users.noreply.github.com>
2025-05-31 14:52:00 -0300

    Iniciando o carregamento dinâmico

--- 7.REQ_HTTP/httpreact/src/App.jsx
+++ 7.REQ_HTTP/httpreact/src/App.jsx
@@ -7,6 +7,11 @@ const url = "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+
+
   // 1 - RESGATANDO DADOS
   useEffect(() => {
     async function fetchData() {
@@ -19,10 +24,29 @@ function App() {
       }
     }
 
-    fetchData();
+    fetchData(); 
   }, []);
 
-  console.log(products);
+  // 2 - ADD PRODUTOS
+  const handleSubmit = async (e) => {
+    e.preventDefault() 
+
+    const product = {
+      name,
+      price
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(product),
+    });
+
+    
+  }
+
 
   return (
     <div className="App">
@@ -34,6 +58,19 @@ function App() {
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar Produto" />
+        </form>
+      </div>
     </div>
   );
 }
