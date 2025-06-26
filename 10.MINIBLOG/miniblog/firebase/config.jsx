6ca5bb24506299d9226ad6f4b4627aa171150b34
Kauan Lima <kauanalmeidalima1405@gmail.com>
2025-06-26 20:08:11 -0300

    Iniciando projeto de MiniBlog

---
+++ 10.MINIBLOG/miniblog/firebase/config.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9ULsmJLrbRdDArepl3EXLVqMsMrTLKXs",
+  authDomain: "miniblog-3bbe6.firebaseapp.com",
+  projectId: "miniblog-3bbe6",
+  storageBucket: "miniblog-3bbe6.firebasestorage.app",
+  messagingSenderId: "781982136908",
+  appId: "1:781982136908:web:8657aaa26aab277cf3d2e9",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
